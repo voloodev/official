67540c06f6cd1a660718740c3ac5f9097772f325
voloodev <dhanulyasas200611@gmail.com>
2026-04-24 19:57:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNJJYShOWoQ85Gh-133VEv1NbH4xIO_Pk",
+  authDomain: "voloo-7123e.firebaseapp.com",
+  projectId: "voloo-7123e",
+  storageBucket: "voloo-7123e.firebasestorage.app",
+  messagingSenderId: "1064367375275",
+  appId: "1:1064367375275:web:1e619ba6b7620c40b208ee",
+  measurementId: "G-VK3CK8LT5E"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+document.getElementById('login-btn').addEventListener('click', () => {
+    signInWithPopup(auth, provider).then((result) => {
+        if (result.user.email === "dhanulyasas200611@gmail.com") {
+            alert("Admin Access Granted!");
+            // මෙතනදී ඔබට Edit buttons පෙන්වීමට කෝඩ් එක ලියන්න පුළුවන්
+        } else {
+            alert("Welcome user!");
+        }
+    });
+});
